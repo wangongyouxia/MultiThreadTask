2025c53c524ef1e1d22eb47d84b8913b582b05f2
zuhaoli <709865788@qq.com>
2018-06-21 21:11:08 +0800

    fix bug

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-import MultiThreadTask
+import MultiThreadTask as mtt
 import os
 import time
 
@@ -30,18 +30,18 @@ if __name__ == '__main__':
 
 	# A case with parameters and callback. WARN: callback function must accept ONE parameter!
 	params_list = [(1,2),(2,3),(3,4),(4,5),(5,6),(7,8)]
-	task = MultiThreadTask.MultiThreadTask(func=add,thread_num=3,params_list=params_list,callback=callback_add)
+	task = mtt.MultiThreadTask(func=add,thread_num=3,params_list=params_list,callback=callback_add)
 	task.start()
 
 	# A case with parameters. WARN: Every element in paramerter list must be iterable. You can use list or tuple. A WRONG case is [1,2], since 1 and 2 is not iterable.
 	params_list = [(1,),(2,),(3,),(4,)]
-	task = MultiThreadTask.MultiThreadTask(func=display_info,thread_num=3,params_list=params_list)
+	task = mtt.MultiThreadTask(func=display_info,thread_num=3,params_list=params_list)
 	task.start()
 
 	# A case without parameter, task_num means how many times do you want to run).
-	task = MultiThreadTask.MultiThreadTask(func=print_i_want_to_sleep, thread_num=4, no_params=True,task_num=6)
+	task = mtt.MultiThreadTask(func=print_i_want_to_sleep, thread_num=4, no_params=True,task_num=6)
 	task.start()
 
 	# A case without parameters, work endlessly(task_number = -1 means endless work).
-	task = MultiThreadTask.MultiThreadTask(func=print_i_need_money, thread_num=4, no_params=True,task_num=-1)
+	task = mtt.MultiThreadTask(func=print_i_need_money, thread_num=4, no_params=True,task_num=-1)
 	task.start()
